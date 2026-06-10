2b4d18aca96cf6643efd84a5bdd2fba06f38e4f4
Harrison Chapman <hchap1@hotmail.com>
2026-06-10 21:00:19 +1000

    chore: tests in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub mod authentication;
 pub mod error;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let permanent_token = authentication::oauth2::wrapper::acquire_refresh_token().await.unwrap();
+    let access_token = authentication::oauth2::wrapper::acquire_session_token(permanent_token).await.unwrap().unwrap();
+    println!("AccessToken: {}", access_token.access_token);
 }
